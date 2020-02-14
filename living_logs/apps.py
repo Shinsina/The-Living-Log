fae21bc4b2db5e8c44332915a77c36b1dff60c88
shinsina <shinsina@att.net>
2020-02-14 17:27:39 -0600

    Ready for deployment to heroku.

---
+++ living_logs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LivingLogsConfig(AppConfig):
+    name = 'living_logs'
---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\collinsjacob\python scripts\learning log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
